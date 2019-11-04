e80a9af320c09941722a660f08665de36ad495b4
prezrohit <rohitsr80@gmail.com>
2019-11-04 20:33:38 +0530

    SecureBaseURL added

--- app/src/main/java/in/prepskool/prepskoolacademy/utils/Endpoints.java
+++ app/src/main/java/in/prepskool/prepskoolacademy/utils/Endpoints.java
@@ -2,7 +2,7 @@ package in.prepskool.prepskoolacademy.utils;
 
 public class Endpoints {
 
-    public static final String BASE_URL = "http://prepskool.in/app/api/v1/";
+    public static final String BASE_URL = "https://prepskool.in/app/api/v1/";
 
     public static final String GOOGLEDRIVE = "https://drive.google.com/uc?export=download&id=";
 }
